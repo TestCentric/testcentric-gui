831772418c3c9e5d6a839b0ae11b939dd8ac489d
Charlie Poole <charliepoole@gmail.com>
2018-03-15 10:03:17 -0700

    Correct help text. Fixes #10.

--- src/TestCentric/testcentric.gui/CommandLineOptions.cs
+++ src/TestCentric/testcentric.gui/CommandLineOptions.cs
@@ -150,7 +150,7 @@ namespace TestCentric.Gui
         public string GetHelpText()
         {
             var writer = new StringWriter();
-            writer.WriteLine("TC [inputfiles] [options]");
+            writer.WriteLine("TC-NUnit [inputfiles] [options]");
             writer.WriteLine();
             writer.WriteLine("Starts the TestCentric Runner, optionally loading and running a set of NUnit tests. You may specify any combination of assemblies and supported project files as arguments.");
             writer.WriteLine();
