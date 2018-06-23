5e8d7c3b528d25beb92be69a48c3e339b097a518
Mikkel Nylander Bundgaard <mikkelbu@gmail.com>
2018-06-23 22:04:22 +0200

    Add test of RecentFileMenuHandler
    Reintroduced RecentFileMenuHandlerTests.cs and added tests of the
    filtering functionality. The mocking of the settings could be nicer,
    but that will require that we introduce interfaces for all settings
    classes under `UserSettings`.

--- src/TestCentric/tests/RecentFileMenuHandlerTests.cs
+++ src/TestCentric/tests/RecentFileMenuHandlerTests.cs
@@ -1,100 +1,166 @@
-// ****************************************************************
-// Copyright 2002-2003, Charlie Poole
-// This is free software licensed under the NUnit license. You may
-// obtain a copy of the license at http://nunit.org
-// ****************************************************************
+// ***********************************************************************
+// Copyright (c) 2018 Charlie Poole
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ***********************************************************************
 
-using System;
-using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
+using NSubstitute;
 using NUnit.Engine;
 using NUnit.Framework;
+using TestCentric.Gui.Model;
+using TestCentric.Gui.Model.Settings;
 
-namespace NUnit.Gui.Tests
+namespace TestCentric.Gui.Tests
 {
-	[TestFixture]
-	public class RecentFileMenuHandlerTests
-	{
-		private MenuItem menu;
-		private IRecentFiles files;
-		private RecentFileMenuHandler handler;
-		
-		[SetUp]
-		public void SetUp()
-		{
-			menu = new MenuItem();
-			files = new FakeRecentFiles();
-			handler = new RecentFileMenuHandler( menu, files );
-            handler.CheckFilesExist = false;
+    [TestFixture]
+    public class RecentFileMenuHandlerTests
+    {
+        private MenuItem menu;
+        private IRecentFiles files;
+        private RecentFileMenuHandler handler;
+        private int maxFiles;
+        private bool checkFilesExist;
+
+        [SetUp]
+        public void SetUp()
+        {
+            menu = new MenuItem();
+            var testModel = Substitute.For<ITestModel>();
+            var settings = Substitute.For<ISettings>();
+            maxFiles = 24;
+            settings.GetSetting("Gui.RecentProjects.MaxFiles", Arg.Any<int>()).Returns(_ => maxFiles);
+            checkFilesExist = false;
+            settings.GetSetting("Gui.RecentProjects.CheckFilesExist", Arg.Any<bool>()).Returns(_ => checkFilesExist);
+            var userSettings = new UserSettings(settings);
+            testModel.Services.UserSettings.Returns(userSettings);
+
+            files = new FakeRecentFiles();
+            testModel.Services.RecentFiles.Returns(files);
+
+            handler = new RecentFileMenuHandler(menu, testModel, f => !f.StartsWith("_"));
         }
 
-		[Test]
-		public void DisableOnLoadWhenEmpty()
-		{
-			handler.Load();
-			Assert.IsFalse( menu.Enabled );
-		}
+        [Test]
+        public void DisableOnLoadWhenEmpty()
+        {
+            handler.Load();
+            Assert.IsFalse(menu.Enabled);
+        }
 
-		[Test]
-		public void EnableOnLoadWhenNotEmpty()
-		{
-			files.SetMostRecent( "Test" );
-			handler.Load();
-			Assert.IsTrue( menu.Enabled );
-		}
-		[Test]
-		public void LoadMenuItems()
-		{
-			files.SetMostRecent( "Third" );
-			files.SetMostRecent( "Second" );
-			files.SetMostRecent( "First" );
-			handler.Load();
-			Assert.AreEqual( 3, menu.MenuItems.Count );
-			Assert.AreEqual( "1 First", menu.MenuItems[0].Text );
-		}
+        [Test]
+        public void EnableOnLoadWhenNotEmpty()
+        {
+            files.SetMostRecent("Test");
+            handler.Load();
+            Assert.IsTrue(menu.Enabled);
+        }
 
-		private class FakeRecentFiles : RecentFiles
-		{
-			private RecentFilesCollection files = new RecentFilesCollection();
-			private int maxFiles = 24;
+        [Test]
+        public void LoadMenuItems()
+        {
+            files.SetMostRecent("Third");
+            files.SetMostRecent("Second");
+            files.SetMostRecent("First");
+            handler.Load();
+            Assert.AreEqual(3, menu.MenuItems.Count);
+            Assert.AreEqual("1 First", menu.MenuItems[0].Text);
+        }
 
-			public int Count
-			{
-				get { return files.Count; }
-			}
+        [Test]
+        public void LoadMoreMenuItemsThanMaxFiles([Values(1, 2, 3)] int numMaxFiles)
+        {
+            maxFiles = numMaxFiles;
+            files.SetMostRecent("Fourth");
+            files.SetMostRecent("Third");
+            files.SetMostRecent("Second");
+            files.SetMostRecent("First");
+            handler.Load();
+            Assert.That(menu.MenuItems.Count, Is.EqualTo(numMaxFiles));
+        }
 
-			public int MaxFiles
-			{
-				get { return maxFiles; }
-				set { maxFiles = value; }
-			}
+        [TestCase(true, new[] { "1 First", "2 Second", "3 Third" })]
+        [TestCase(false, new[] { "1 First", "2 _ First filter", "3 Second", "4 _ Second filter", "5 Third" })]
+        public void LoadMenuItemsFilteringWorks(bool checkFiles, string[] expectedItems)
+        {
+            checkFilesExist = checkFiles;
+            files.SetMostRecent("Third");
+            files.SetMostRecent("_ Second filter");
+            files.SetMostRecent("Second");
+            files.SetMostRecent("_ First filter");
+            files.SetMostRecent("First");
+            handler.Load();
+            Assert.AreEqual(expectedItems.Count(), menu.MenuItems.Count);
+            CollectionAssert.AreEqual(expectedItems, menu.MenuItems.Cast<MenuItem>().Select(m => m.Text));
+        }
 
-			public void SetMostRecent( string fileName )
-			{
-				SetMostRecent( new RecentFileEntry( fileName ) );
-			}
+        [TestCase(true, new[] { "1 First", "2 Second" })]
+        [TestCase(false, new[] { "1 First", "2 _ First filter" })]
+        public void LoadMenuItemsFilteringWorksWithTooManyElements(bool checkFiles, string[] expectedItems)
+        {
+            maxFiles = 2;
+            checkFilesExist = checkFiles;
+            files.SetMostRecent("Third");
+            files.SetMostRecent("_ Second filter");
+            files.SetMostRecent("Second");
+            files.SetMostRecent("_ First filter");
+            files.SetMostRecent("First");
+            handler.Load();
+            Assert.AreEqual(expectedItems.Count(), menu.MenuItems.Count);
+            CollectionAssert.AreEqual(expectedItems, menu.MenuItems.Cast<MenuItem>().Select(m => m.Text));
+        }
 
-			public void SetMostRecent( RecentFileEntry entry )
-			{
-				files.Insert( 0, entry );
-			}
+        private class FakeRecentFiles : IRecentFiles
+        {
+            private IList<string> files = new List<string>();
+            private int maxFiles = 24;
 
-			public RecentFilesCollection Entries
-			{
-				get { return files; }
-			}
+            public int Count
+            {
+                get { return files.Count; }
+            }
 
-			public void Clear()
-			{
-				files.Clear();
-			}
+            public int MaxFiles
+            {
+                get { return maxFiles; }
+                set { maxFiles = value; }
+            }
 
-			public void Remove( string fileName )
-			{
-				files.Remove( fileName );
-			}
-		}
-	
-		// TODO: Need mock loader to test clicking
-	}
+            public void SetMostRecent(string fileName)
+            {
+                files.Insert(0, fileName);
+            }
+
+            public IList<string> Entries
+            {
+                get { return files; }
+            }
+
+            public void Remove(string fileName)
+            {
+                files.Remove(fileName);
+            }
+        }
+
+        // TODO: Need mock loader to test clicking
+    }
 }
