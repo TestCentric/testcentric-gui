70db69a244255061202e50fc79d8f10e2c8f219a
stefano.nuzzo@omicronelectronic.com <stefano.nuzzo@omicronenergy.com>
2018-10-26 00:48:32 +0200

    port of AssemblyWatcher from NUnit2

---
+++ src/TestModel/model/AssemblyWatcher.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+
+namespace TestCentric.Gui.Model
+{
+    public class AssemblyWatcher:IAsemblyWatcher
+    {
+        private FileSystemWatcher[] fileWatchers;
+        private FileInfo[] files;
+
+        protected System.Timers.Timer timer;
+        protected string changedAssemblyPath;
+
+        protected FileInfo GetFileInfo(int index)
+        {
+            return files[index];
+        }
+
+        public void Setup(int delay, string assemblyFileName)
+        {
+            Setup(delay, new string[] { assemblyFileName });
+        }
+
+#if CLR_2_0 || CLR_4_0
+        public void Setup(int delay, System.Collections.Generic.IList<string> assemblies)
+#else
+        public void Setup(int delay, System.Collections.IList assemblies)
+#endif
+        {
+
+            files = new FileInfo[assemblies.Count];
+            fileWatchers = new FileSystemWatcher[assemblies.Count];
+
+            for (int i = 0; i < assemblies.Count; i++)
+            {
+
+                files[i] = new FileInfo((string)assemblies[i]);
+
+                fileWatchers[i] = new FileSystemWatcher();
+                fileWatchers[i].Path = files[i].DirectoryName;
+                fileWatchers[i].Filter = files[i].Name;
+                fileWatchers[i].NotifyFilter = NotifyFilters.Size | NotifyFilters.LastWrite;
+                fileWatchers[i].Changed += new FileSystemEventHandler(OnChanged);
+                fileWatchers[i].EnableRaisingEvents = false;
+            }
+
+            timer = new System.Timers.Timer(delay);
+            timer.AutoReset = false;
+            timer.Enabled = false;
+            timer.Elapsed += new ElapsedEventHandler(OnTimer);
+        }
+
+        public void Start()
+        {
+            EnableWatchers(true);
+        }
+
+        public void Stop()
+        {
+            EnableWatchers(false);
+        }
+
+        private void EnableWatchers(bool enable)
+        {
+            if (fileWatchers != null)
+                foreach (FileSystemWatcher watcher in fileWatchers)
+                    watcher.EnableRaisingEvents = enable;
+        }
+
+        public void FreeResources()
+        {
+
+            Stop();
+
+            if (fileWatchers != null)
+            {
+                foreach (FileSystemWatcher watcher in fileWatchers)
+                {
+                    if (watcher != null)
+                    {
+                        watcher.Changed -= new FileSystemEventHandler(OnChanged);
+                        watcher.Dispose();
+                    }
+                }
+            }
+
+            if (timer != null)
+            {
+                timer.Stop();
+                timer.Close();
+            }
+
+            fileWatchers = null;
+            timer = null;
+        }
+
+        public event AssemblyChangedHandler AssemblyChanged;
+
+        protected void OnTimer(Object source, ElapsedEventArgs e)
+        {
+            lock (this)
+            {
+                PublishEvent();
+                timer.Enabled = false;
+            }
+        }
+
+        protected void OnChanged(object source, FileSystemEventArgs e)
+        {
+
+            changedAssemblyPath = e.FullPath;
+            if (timer != null)
+            {
+                lock (this)
+                {
+                    if (!timer.Enabled)
+                        timer.Enabled = true;
+                    timer.Start();
+                }
+            }
+            else
+            {
+                PublishEvent();
+            }
+        }
+
+        protected void PublishEvent()
+        {
+            if (AssemblyChanged != null)
+            {
+                AssemblyChanged(changedAssemblyPath);
+            }
+        }
+    }
+}
---
+++ src/TestModel/model/IAsemblyWatcher.cs
@@ -0,0 +1,46 @@
+﻿namespace TestCentric.Gui.Model
+{
+    public delegate void AssemblyChangedHandler(string fullPath);
+    public interface IAsemblyWatcher
+    {
+        /// <summary>
+        /// Stops watching for changes.
+        /// To release resources call FreeResources.
+        /// </summary>
+        void Stop();
+
+        /// <summary>
+        /// Starts watching for assembly changes.
+        /// You need to call Setup before start watching.
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Initializes the watcher with assemblies to observe for changes.
+        /// </summary>
+        /// <param name="delayInMs">The delay in ms.</param>
+        /// <param name="assemblies">The assemblies.</param>
+#if CLR_2_0 || CLR_4_0
+        void Setup(int delayInMs, System.Collections.Generic.IList<string> assemblies);
+#else
+        void Setup(int delayInMs, System.Collections.IList assemblies);
+#endif
+
+        /// <summary>
+        /// Initializes the watcher with assemblies to observe for changes.
+        /// </summary>
+        /// <param name="delayInMs">The delay in ms.</param>
+        /// <param name="assemblyFileName">Name of the assembly file.</param>
+        void Setup(int delayInMs, string assemblyFileName);
+
+        /// <summary>
+        /// Releases all resources held by the watcher.
+        /// </summary>
+        void FreeResources();
+
+        /// <summary>
+        /// Occurs when an assembly being watched has changed.
+        /// </summary>
+        event AssemblyChangedHandler AssemblyChanged;
+    }
+}
