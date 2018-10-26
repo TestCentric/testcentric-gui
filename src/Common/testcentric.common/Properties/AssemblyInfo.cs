d7f4bf3bb2b3fdd485e5c29047fb9657f954425c
Charlie Poole <charliepoole@gmail.com>
2018-10-26 22:48:33 +0100

    Create new common assembly

---
+++ src/Common/testcentric.common/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("TestCentric.Common")]
+[assembly: AssemblyDescription("Types used by both the model and the GUI itself")]
