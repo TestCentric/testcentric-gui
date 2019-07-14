bc13daf7c2201441e2f09d9414558e6d6ee89fd5
Charlie Poole <charliepoole@gmail.com>
2019-07-14 04:22:09 +0100

    Update version for beta3 release

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -46,4 +46,4 @@ using System.Reflection;
 // by using the '*' as shown below:
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-Beta2")]
+[assembly: AssemblyInformationalVersion("1.0.0-Beta3")]
