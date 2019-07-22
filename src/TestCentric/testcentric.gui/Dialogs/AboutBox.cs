a85099584a7530a3ab388906eaac182052b076fd
Charlie Poole <charliepoole@gmail.com>
2019-07-22 17:51:20 +0100

    Add testcentric images and use one in about box

--- src/TestCentric/testcentric.gui/Dialogs/AboutBox.cs
+++ src/TestCentric/testcentric.gui/Dialogs/AboutBox.cs
@@ -169,7 +169,7 @@ namespace TestCentric.Gui
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(14, 11);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(130, 132);
+            this.pictureBox1.Size = new System.Drawing.Size(135, 132);
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
             // 
