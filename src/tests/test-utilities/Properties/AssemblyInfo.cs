3a63cfe1b9ff91ea356f1a3a467f5d4689557891
Robert Snyder <immeraufdemhund@hotmail.com>
2019-12-24 09:10:58 -0600

    Update Copyright headers in all files in tests
    #466

--- src/tests/test-utilities/Properties/AssemblyInfo.cs
+++ src/tests/test-utilities/Properties/AssemblyInfo.cs
@@ -1,1 +1,6 @@
-﻿[assembly: NUnit.Framework.NonTestAssembly]
+﻿// ***********************************************************************
+// Copyright (c) Charlie Poole and TestCentric GUI contributors.
+// Licensed under the MIT License. See LICENSE.txt in root directory.
+// ***********************************************************************
+
+[assembly: NUnit.Framework.NonTestAssembly]
