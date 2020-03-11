514b706f3790f314b452589768baeb22f7eacc8b
Charlie Poole <charliepoole@gmail.com>
2020-03-11 12:08:04 -0700

    Standardize AssemblyInfo and versioning

--- src/TestEngine/CommonEngineAssemblyInfo.cs
+++ src/TestEngine/CommonEngineAssemblyInfo.cs
@@ -7,34 +7,20 @@ using System.Reflection;
 
 //
 // Common Information for the TestCentric Engine 
-
 //
+
+[assembly: AssemblyProduct("TestCentric Engine")]
+
+[assembly: AssemblyCompany("TestCentric Software")]
+[assembly: AssemblyCopyright("Copyright (c) 2019-2020 Charlie Poole and TestCentric Engine contributors")]
 [assembly: AssemblyTrademark("TestCentric is a trademark of TestCentric Software")]
-[assembly: AssemblyCompany("TestCentric")]
-[assembly: AssemblyCopyright("Copyright (c) 2019 Charlie Poole and TestCentric Engine contributors")]
 
 #if DEBUG
-#if NET35
-[assembly: AssemblyConfiguration(".NET 3.5 Debug")]
-#elif NET20
-[assembly: AssemblyConfiguration(".NET 2.0 Debug")]
-#elif NETSTANDARD1_6 || NETCOREAPP1_1
-[assembly: AssemblyConfiguration(".NET Standard 1.6 Debug")]
-#elif NETSTANDARD2_0 || NETCOREAPP2_1
-[assembly: AssemblyConfiguration(".NET Standard 2.0 Debug")]
-#else
 [assembly: AssemblyConfiguration("Debug")]
-#endif
 #else
-#if NET35
-[assembly: AssemblyConfiguration(".NET 3.5")]
-#elif NET20
-[assembly: AssemblyConfiguration(".NET 2.0")]
-#elif NETSTANDARD1_6 || NETCOREAPP1_1
-[assembly: AssemblyConfiguration(".NET Standard 1.6")]
-#elif NETSTANDARD2_0 || NETCOREAPP2_1
-[assembly: AssemblyConfiguration(".NET Standard 2.0")]
-#else
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyConfiguration("Release")]
 #endif
-#endif
+
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1")]
+[assembly: AssemblyInformationalVersion("1.3.1")]
--- src/TestEngine/testcentric.engine.core.tests/Properties/AssemblyInfo.cs
+++ src/TestEngine/testcentric.engine.core.tests/Properties/AssemblyInfo.cs
@@ -6,17 +6,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("tests")]
 [assembly: AssemblyDescription("Tests of the NUnit Engine")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6CD6E5B1-E41A-41CE-8B4C-C5749C8536BB")]
