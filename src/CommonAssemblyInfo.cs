6ea7ed079a8d5e8e70a91011c8b3a8ff21bbbe2f
Charlie Poole <charliepoole@gmail.com>
2020-04-17 17:31:47 -0700

    Prepare 1.3.3 Release

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -23,4 +23,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.3.3.0")]
 [assembly: AssemblyFileVersion("1.3.3")]
-[assembly: AssemblyInformationalVersion("1.3.3-ci00021-issue-562")]
+[assembly: AssemblyInformationalVersion("1.3.3-rc1")]
