a9421ddc7c719f1c531fe6462bf4bc2a17119a6f
Charlie Poole <charliepoole@gmail.com>
2020-12-12 19:02:44 -0800

    Fix broken build

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0")]
-[assembly: AssemblyInformationalVersion("1.5.0")]
+[assembly: AssemblyVersion("1.5.1.0")]
+[assembly: AssemblyFileVersion("1.5.1")]
+[assembly: AssemblyInformationalVersion("1.5.1-ci00017-issue-324b")]
