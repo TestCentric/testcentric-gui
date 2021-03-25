cfebf400ffec2f2879fca4c0b39247f19992b999
Charlie Poole <charliepoole@gmail.com>
2021-03-24 17:29:25 -0700

    Update Changes and version

--- src/TestEngine/CommonEngineAssemblyInfo.cs
+++ src/TestEngine/CommonEngineAssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("1.6.1.0")]
-[assembly: AssemblyFileVersion("1.6.1")]
-[assembly: AssemblyInformationalVersion("1.6.1")]
+[assembly: AssemblyVersion("1.6.2.0")]
+[assembly: AssemblyFileVersion("1.6.2")]
+[assembly: AssemblyInformationalVersion("1.6.2")]
