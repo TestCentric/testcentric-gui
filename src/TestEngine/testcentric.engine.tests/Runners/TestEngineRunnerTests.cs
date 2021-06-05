dfe8d0044a73477462f0090101ee764ef37e9df1
Charlie Poole <charliepoole@gmail.com>
2021-06-05 09:25:26 -0700

    Change ProcessRunner to AssemblyRunner; Add new interface to TestAgency

--- src/TestEngine/testcentric.engine.tests/Runners/TestEngineRunnerTests.cs
+++ src/TestEngine/testcentric.engine.tests/Runners/TestEngineRunnerTests.cs
@@ -120,7 +120,7 @@ namespace TestCentric.Engine.Runners
         public void RunAsync()
         {
 #if !NETCOREAPP2_1
-            if (_runner is ProcessRunner || _runner is AggregatingTestRunner )
+            if (_runner is AssemblyRunner || _runner is AggregatingTestRunner )
                 Assert.Ignore("RunAsync is not working for ProcessRunner");
 #endif
 
