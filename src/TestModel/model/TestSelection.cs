84b727b5500217474caa31a534d5f9e76815f536
Charlie Poole <charliepoole@gmail.com>
2021-07-03 11:54:36 -0700

    Add NUnitTreeDisplayStrategy

--- src/TestModel/model/TestSelection.cs
+++ src/TestModel/model/TestSelection.cs
@@ -11,14 +11,10 @@ namespace TestCentric.Gui.Model
 {
     public delegate string GroupingFunction(TestNode testNode);
 
-    // TODO: We are now building for .NET 4.5 so the following
-    // limitation no longer applies.
-
     /// <summary>
-    /// TestSelection is an extended List of TestNodes, with
-    /// the addition of a SortBy method. Since the Gui is 
-    /// designed to run under the .NET 2.0 runtime or later,
-    /// we can't use the extension methods for List.
+    /// TestSelection is an List of TestNodes, implementing
+    /// the ITestItem interface so that the entire selection
+    /// may be selected for execution.
     /// </summary>
     public class TestSelection : List<TestNode>, ITestItem
     {
