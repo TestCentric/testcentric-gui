296e4c7796e58d40500bcc81b7033e5967c13dd0
Charlie Poole <charliepoole@gmail.com>
2021-09-03 20:29:57 -0700

    Add basic tests of AgentOptions

---
+++ src/TestEngine/agents/testcentric.agent.core.tests/Agents/AgentOptionTests.cs
@@ -0,0 +1,58 @@
+// ***********************************************************************
+// Copyright (c) Charlie Poole and TestCentric GUI contributors.
+// Licensed under the MIT License. See LICENSE file in root directory.
+// ***********************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Engine;
+using NUnit.Framework;
+
+namespace TestCentric.Engine.Agents
+{
+    public class AgentOptionTests
+    {
+        static TestCaseData[] DefaultSettings = new[]
+        {
+            new TestCaseData("AgentId", Guid.Empty),
+            new TestCaseData("AgencyUrl", string.Empty),
+            new TestCaseData("AgencyPid", string.Empty),
+            new TestCaseData("DebugAgent", false),
+            new TestCaseData("DebugTests", false),
+            new TestCaseData("TraceLevel", InternalTraceLevel.Off),
+            new TestCaseData("WorkDirectory", string.Empty)
+        };
+
+        [TestCaseSource(nameof(DefaultSettings))]
+        public void DefaultOptionSettings<T>(string propertyName, T defaultValue)
+        {
+            var options = new AgentOptions();
+            var prop = typeof(AgentOptions).GetProperty(propertyName);
+            Assert.NotNull(prop, $"Property {propertyName} does not exist");
+            Assert.That(prop.GetValue(options, new object[0]), Is.EqualTo(defaultValue));
+        }
+
+        static readonly Guid AGENT_GUID = Guid.NewGuid();
+        static readonly TestCaseData[] ValidSettings = new[]
+        {
+            new TestCaseData($"--agentId={AGENT_GUID}", "AgentId", AGENT_GUID),
+            new TestCaseData("--agencyUrl=THEURL", "AgencyUrl", "THEURL"),
+            new TestCaseData("--pid=1234", "AgencyPid", "1234"),
+            new TestCaseData("--debug-agent", "DebugAgent", true),
+            new TestCaseData("--debug-tests", "DebugTests", true),
+            new TestCaseData("--trace=Info", "TraceLevel", InternalTraceLevel.Info),
+            new TestCaseData("--work=WORKDIR", "WorkDirectory", "WORKDIR")
+        };
+
+        [TestCaseSource(nameof(ValidSettings))]
+        public void ValidOptionSettings<T>(string option, string propertyName, T expectedValue)
+        {
+            var options = new AgentOptions(option);
+            var prop = typeof(AgentOptions).GetProperty(propertyName);
+            Assert.NotNull(prop, $"Property {propertyName} does not exist");
+            Assert.That(prop.GetValue(options, new object[0]), Is.EqualTo(expectedValue));
+        }
+    }
+}
