09511b1b84664a9f07ccdc11333301562c3bed97
Charlie Poole <charliepoole@gmail.com>
2022-06-05 12:56:43 -0700

    Add --timeout option

--- src/TestBed/CommandLineOptions.cs
+++ src/TestBed/CommandLineOptions.cs
@@ -15,31 +15,51 @@ namespace TestCentric.Engine.TestBed
             foreach (string arg in args)
             {
                 if (arg.StartsWith("-"))
-                    switch (arg)
-                    {
-                        case "--trace":
-                            Trace = true;
-                            break;
-
-                        case "--debug-agent":
-                            DebugAgent = true;
-                            break;
-
-                        case "--list-extensions":
-                            ListExtensions = true;
-                            break;
-
-                        default:
-                            throw new ArgumentException($"Invalid option: '{arg}'");
-                    }
+                    ProcessOption(arg);
                 else
                     Files.Add(arg);
             }
         }
 
         public List<String> Files { get; } = new List<string>();
-        public bool DebugAgent { get; }
-        public bool Trace { get; }
-        public bool ListExtensions { get; }
+        public bool DebugAgent { get; private set; }
+        public bool Trace { get; private set; }
+        public bool ListExtensions { get; private set; }
+        public int Timeout { get; private set; } = 0;
+
+        private void ProcessOption(string arg)
+        {
+            string opt = arg;
+            string val = null;
+
+            int eq = arg.IndexOf('=');
+            if (eq > 0)
+            {
+                opt = arg.Substring(0, eq);
+                val = arg.Substring(eq + 1);
+            }
+
+            switch (opt)
+            {
+                case "--trace":
+                    Trace = true;
+                    break;
+
+                case "--debug-agent":
+                    DebugAgent = true;
+                    break;
+
+                case "--list-extensions":
+                    ListExtensions = true;
+                    break;
+
+                case "--timeout":
+                    Timeout = int.Parse(val);
+                    break;
+
+                default:
+                    throw new ArgumentException($"Invalid option: '{arg}'");
+            }
+        }
     }
 }
