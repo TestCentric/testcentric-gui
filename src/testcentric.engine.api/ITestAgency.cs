7ee46ba1043d81c92c295aedff535165b853bcd7
Charlie Poole <charliepoole@gmail.com>
2023-11-12 09:03:11 -0800

    Move some Types from engine.core

---
+++ src/testcentric.engine.api/ITestAgency.cs
@@ -0,0 +1,20 @@
+// ***********************************************************************
+// Copyright (c) Charlie Poole and TestCentric contributors.
+// Licensed under the MIT License. See LICENSE file in root directory.
+// ***********************************************************************
+
+namespace TestCentric.Engine
+{
+    /// <summary>
+    /// The ITestAgency interface is implemented by a TestAgency in 
+    /// order to allow TestAgents to register with it.
+    /// </summary>
+    public interface ITestAgency
+    {
+        /// <summary>
+        /// Registers an agent with an agency
+        /// </summary>
+        /// <param name="agent"></param>
+        void Register(ITestAgent agent);
+    }
+}
