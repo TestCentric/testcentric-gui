6c9bf423085f93be310f371efeb0a3f21bf820d3
Charlie Poole <charliepoole@gmail.com>
2023-12-17 06:56:49 -0800

    Add FilterTests (moved from TestCentric.Engine.Core)

---
+++ src/TestCentric.Engine.Api.Tests/TestFilterTests.cs
@@ -0,0 +1,35 @@
+// ***********************************************************************
+// Copyright (c) Charlie Poole and TestCentric contributors.
+// Licensed under the MIT License. See LICENSE file in root directory.
+// ***********************************************************************
+
+using NUnit.Framework;
+
+namespace TestCentric.Engine.Api
+{
+    public class TestFilterTests
+    {
+        [Test]
+        public void EmptyFilter()
+        {
+            TestFilter filter = TestFilter.Empty;
+            Assert.That(filter.Text, Is.EqualTo("<filter/>"));
+        }
+
+        [Test]
+        public void FilterWithOneTest()
+        {
+            string text = "<filter><tests><test>My.Test.Name</test></tests></filter>";
+            TestFilter filter = new TestFilter(text);
+            Assert.That(filter.Text, Is.EqualTo(text));
+        }
+
+        [Test]
+        public void FilterWithThreeTests()
+        {
+            string text = "<filter><tests><test>My.First.Test</test><test>My.Second.Test</test><test>My.Third.Test</test></tests></filter>";
+            TestFilter filter = new TestFilter(text);
+            Assert.That(filter.Text, Is.EqualTo(text));
+        }
+    }
+}
