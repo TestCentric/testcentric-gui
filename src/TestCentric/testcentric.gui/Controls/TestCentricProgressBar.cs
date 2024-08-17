a74c7eb47a3be57dba470732db425d0e7aab30db
Roald Wolff <roald.wolff@gmail.com>
2024-08-17 07:22:06 +0200

    Improve robustness of ProgressBar + tests

--- src/TestCentric/testcentric.gui/Controls/TestCentricProgressBar.cs
+++ src/TestCentric/testcentric.gui/Controls/TestCentricProgressBar.cs
@@ -64,20 +64,31 @@ namespace TestCentric.Gui.Controls
 
         #region Methods
 
+        public int GetProgressWidth(Rectangle rec)
+        {
+            if (Maximum == 0)
+                return 0;
+            return (int)(rec.Width * ((double)Value / Maximum));
+        }
+
+        public int GetPercentCompleted()
+        {
+            if (Maximum == 0)
+                return 0;
+            return Value * 100 / Maximum;
+        }
+
         protected override void OnPaint(PaintEventArgs e)
         {
-            if (Maximum == 0)       // No progress can be painted in this case
-                return;
-
             Rectangle rec = this.ClientRectangle;
             rec.Inflate(-2, -2);
             if (ProgressBarRenderer.IsSupported)
                 ProgressBarRenderer.DrawHorizontalBar(e.Graphics, rec);
             rec.Inflate(-1, -1);
-            rec.Width = (int)(rec.Width * ((double)Value / Maximum));
+            rec.Width = GetProgressWidth(rec);
             e.Graphics.FillRectangle(_brush, rec); //2, 2, rec.Width, rec.Height);
 
-            int percentComplete = Value * 100 / Maximum;
+            int percentComplete = GetPercentCompleted();
             if (ShowPercentComplete && percentComplete > 0)
             {
                 string text = $"{(int)percentComplete}%";
