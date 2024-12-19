c84d77571dab3ba0ffc66ab12f836367aa40d070
rowo360 <59574371+rowo360@users.noreply.github.com>
2024-12-19 07:25:46 +0100

    Add UI elements for textual filtering of tests

---
+++ src/TestCentric/testcentric.gui/Elements/TextBoxElement.cs
@@ -0,0 +1,135 @@
+// ***********************************************************************
+// Copyright (c) Charlie Poole and TestCentric contributors.
+// Licensed under the MIT License. See LICENSE file in root directory.
+// ***********************************************************************
+
+
+using System;
+using System.Windows.Forms;
+
+namespace TestCentric.Gui.Elements
+{
+    /// <summary>
+    /// This class implements the ISelection interface for a TextBox control. It provides this additional functionality:
+    /// - show a PlaceHoder text if there's no text input
+    /// - Invoke the SelectionChanged event as soon as no further input is made within a short period of time.
+    /// </summary>
+    public class TextBoxElement : ISelection
+    {
+        private Timer _typingTimer;
+
+        public event CommandHandler SelectionChanged;
+
+        public TextBoxElement(Control textBox, string placeHolderText)
+        {
+            TextBox = textBox;
+            PlaceHolderText = placeHolderText;
+            TextBox.TextChanged += OnTextChanged;
+
+            TextBox.LostFocus += OnTextBoxLostFocus;
+            TextBox.GotFocus += OnTextBoxGotFocus;
+
+            // Call LostFocus to set initial text and color
+            OnTextBoxLostFocus(null, EventArgs.Empty);
+        }
+
+        public string SelectedItem
+        {
+            get => TextBox.Text;
+            set => TextBox.Text = value;
+        }
+
+        public int SelectedIndex
+        {
+            get => 0;
+            set => throw new NotImplementedException();
+        }
+
+        public bool Enabled
+        {
+            get => TextBox.Enabled;
+            set => TextBox.Enabled = value;
+        }
+
+        public bool Visible
+        {
+            get => TextBox.Visible;
+            set => TextBox.Visible = value;
+        }
+
+        public string Text
+        {
+            get => TextBox.Text;
+            set => TextBox.Text = value;
+        }
+
+        private string PlaceHolderText { get; set; }
+
+        private Control TextBox { get; }
+
+        private bool IsPlaceHolderTextShown { get; set; }
+
+        public void InvokeIfRequired(MethodInvoker _delegate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Refresh()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnTextBoxGotFocus(object sender, EventArgs e)
+        {
+            // If the PlaceHolderText is shown, replace it with an empty text
+            if (IsPlaceHolderTextShown)
+            {
+                TextBox.Text = "";
+                TextBox.ForeColor = System.Drawing.Color.Black;
+                IsPlaceHolderTextShown = false;
+            }
+        }
+
+        private void OnTextBoxLostFocus(object sender, EventArgs e)
+        {
+            // If there's no text input, show the PlaceHolderText instead
+            string searchText = TextBox.Text;
+            if (string.IsNullOrEmpty(searchText) && !string.IsNullOrEmpty(PlaceHolderText))
+            {
+                IsPlaceHolderTextShown = true;
+                TextBox.Text = PlaceHolderText;
+                TextBox.ForeColor = System.Drawing.Color.LightGray;
+            }
+        }
+
+        private void OnTextChanged(object sender, EventArgs e)
+        {
+            if (IsPlaceHolderTextShown)
+                return;
+
+            if (_typingTimer == null)
+            {
+                _typingTimer = new Timer();
+                _typingTimer.Interval = 600;
+                _typingTimer.Tick += TypingTimerTimeout;
+            }
+
+            _typingTimer.Stop();
+            _typingTimer.Start();
+        }
+
+        private void TypingTimerTimeout(object sender, EventArgs e)
+        {
+            var timer = sender as Timer;
+            if (timer == null)
+                return;
+
+            // The timer must be stopped!
+            timer.Stop();
+            if (SelectionChanged != null)
+                SelectionChanged();
+
+            TextBox.Focus();
+        }
+    }
+}
